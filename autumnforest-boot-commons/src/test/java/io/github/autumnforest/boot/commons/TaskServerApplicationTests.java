1f97b3e64e53b25ab0813d705d0cc73a9f570543
liao qiulin <1286936364@qq.com>
2023-02-22 00:02:15 +0800

    parent

---
+++ autumnforest-boot-commons/src/test/java/io/github/autumnforest/boot/commons/TaskServerApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.autumnforest.boot.commons;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ autumnforest-boot-result/src/main/java/io/github/autumnforest/boot/commons/R.java
@@ -0,0 +1,8 @@
+package io.github.autumnforest.boot.commons;
+
+
+public class R<T> {
+    private String code;
+    private T data;
+}
+
